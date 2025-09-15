5593a64a2956a55fbd3a929e82719b95bad902c1
graciekeem <sgracekeem@gmail.com>
2025-09-15 09:19:14 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// script.js 파일에 아래 코드를 추가
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const mobileMenu = document.querySelector('.mobile-menu');
+
+    menuToggle.addEventListener('click', function() {
+        mobileMenu.classList.toggle('active');
+    });
+});
